3951051c1ca9f7449d0dba75eadd39c85d59d652
marchidalgo <marc@theleftbit.com>
2026-09-21 11:28:41 +0200

    Use Swift Testing

--- PhoneNumberKitTests/MetadataResourceTests.swift
+++ PhoneNumberKitTests/MetadataResourceTests.swift
@@ -1,13 +1,14 @@
 @testable import PhoneNumberKit
-import XCTest
+import Testing
 
-final class MetadataResourceTests: XCTestCase {
-    func testDefaultMetadataResourceParsesInternationalNumbers() throws {
-        let metadata = try XCTUnwrap(PhoneNumberUtility.defaultMetadataCallback())
+struct MetadataResourceTests {
+    @Test
+    func defaultMetadataResourceParsesInternationalNumbers() throws {
+        let metadata = try #require(try PhoneNumberUtility.defaultMetadataCallback())
         let utility = PhoneNumberUtility(metadataCallback: { metadata })
 
-        XCTAssertEqual(try utility.parse("+34 722 447 302").nationalNumber, 722447302)
-        XCTAssertEqual(try utility.parse("+1 202-555-0125").nationalNumber, 2025550125)
-        XCTAssertEqual(try utility.parse("+55 (11) 91234-5678").nationalNumber, 11912345678)
+        #expect(try utility.parse("+34 722 447 302").nationalNumber == 722447302)
+        #expect(try utility.parse("+1 202-555-0125").nationalNumber == 2025550125)
+        #expect(try utility.parse("+55 (11) 91234-5678").nationalNumber == 11912345678)
     }
 }
